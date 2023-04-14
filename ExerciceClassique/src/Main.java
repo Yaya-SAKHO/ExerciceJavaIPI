6bf46f5d6f75bb22677094d4234363edc9bb9d4b
Yaya Sakho <yahyasakho97a@gmail.com>
2023-04-14 21:38:40 +0200

    branch devlop

--- ExerciceClassique/src/Main.java
+++ ExerciceClassique/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         //Nombre Aleatoire
-        System.out.printf("\nNombre Aleatoire\n");
+        System.out.printf("\nNombre Aleatoir\n");
         NombreAleatoire.nombreAlea();
     }
 }
