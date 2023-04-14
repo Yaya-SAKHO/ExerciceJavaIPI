7d4ed9f00106ffe73b17fc2bfb756856d36558f3
Yaya Sakho <yahyasakho97a@gmail.com>
2023-04-14 21:46:09 +0200

    Rerenverser une Chaine

--- ExerciceClassique/src/Main.java
+++ ExerciceClassique/src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Renverser une chaîne de caractères
+        String chaine = "Bonjour";
+        String renverseChaine = RenverserChaine.renverserChaine(chaine);
+        System.out.printf("Chaine: " + chaine);
+        System.out.printf("\nChaine renverser: " + renverseChaine);
+
+        
         //Nombre Aleatoire
         System.out.printf("\nNombre Aleatoir\n");
         NombreAleatoire.nombreAlea();
---
+++ ExerciceClassique/src/RenverserChaine.java
@@ -0,0 +1,13 @@
+public class RenverserChaine {
+    public static String renverserChaine(String chaine) {
+
+        String resultat = "";
+        int i = 0;
+
+        for (i = chaine.length() - 1; i >= 0; i--) {
+            resultat += chaine.charAt(i);
+        }
+
+        return resultat;
+    }
+}
